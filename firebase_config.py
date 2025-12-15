934f899a352f894b27d45c06fa50397d72c78b2a
Gabriel Costa <159442633+GabbeRique@users.noreply.github.com>
2025-12-15 10:21:06 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
-cred = credentials.Certificate("integra-tech-firebase-adminsdk-fbsvc-91088353d7.json")
+cred = credentials.Certificate("integra-tech-firebase-adminsdk-fbsvc-eae48ae410.json")
 
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
